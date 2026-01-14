9a1bacf43eff17dc48c251f2f9442cac7f92a3e5
tunnus <tunnus86@gmail.com>
2026-01-14 11:56:47 +0100

    core: rm useless feat on toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,5 +19,4 @@ plugins {
     alias(libs.plugins.kotlin.kapt).apply(false)
     alias(libs.plugins.kotlin.parcelize).apply(false)
     alias(libs.plugins.android.library).apply(false)
-    alias(libs.plugins.kotlin.jvm).apply(false)
 }
